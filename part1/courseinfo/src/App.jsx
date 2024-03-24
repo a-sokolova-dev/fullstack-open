251b701e96da1777e4aa0311c182e0e12fe98918
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-03-24 17:14:55 +0200

    feat(part1): complete exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,24 +4,30 @@ import Content from "./Content";
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
-  
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  let totalExercises = parts.reduce((acc, curr) => acc + curr.exercises, 0);
+
   return (
     <div>
       <Header course={course} />
-      <Content exercises={[part1, part2, part3]} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content exercises={parts} />
+      <Total
+        total={totalExercises}
+      />
     </div>
   );
 };
