00af2d9777bf2c22dc3f48b6bd9401138588c0d1
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-03-27 22:36:55 +0200

    feat(part2): complete exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -10,6 +10,11 @@ const App = () => {
       name: newName,
     };
 
+    if (persons.some((p) => p.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     setPersons(persons.concat(nameObject));
     setNewName("");
   };
