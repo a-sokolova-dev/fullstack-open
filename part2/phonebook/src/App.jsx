ad46365c3557a8a20b2e1077d9d12e34b3f924c1
Anna Sokolova <a.sokolova.dev@gmail.com>
2024-03-27 22:57:05 +0200

    feat(part2): complete exercise 2.10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -47,27 +50,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={filter} onChange={handleFilterChange} />
-      </div>
+      <Filter value={filter} onChange={handleFilterChange} />
       <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} />
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm
+        onSubmit={addName}
+        name={newName}
+        onNameChange={handleNameChange}
+        number={newNumber}
+        onNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      {personsToShow.map((p) => (
-        <div key={p.name}>
-          {p.name} {p.number}
-        </div>
-      ))}
+      <Persons persons={personsToShow} />
     </div>
   );
 };
---
+++ part2/phonebook/src/components/Persons.jsx
@@ -0,0 +1,13 @@
+const Persons = ({ persons }) => {
+  return (
+    <>
+      {persons.map((p) => (
+        <div key={p.id}>
+          {p.name} {p.number}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default Persons;
