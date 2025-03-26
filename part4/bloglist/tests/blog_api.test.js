37df313fc15db11e126cf26eb788b31c9fa52b95
Anna Sokolova <a.sokolova.dev@gmail.com>
2025-03-26 17:45:58 +0200

    feat(part4): complete exercises 4.9-4.12

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -37,6 +37,83 @@ test('blogs are returned as json and correct number', async () => {
   assert.strictEqual(response.body.length, initialBlogs.length)
 })
 
+test('blog objects use "id", not "_id"', async () => {
+    const response = await api.get('/api/blogs')
+  
+    for (const blog of response.body) {
+      assert.ok(blog.id, 'blog is missing "id"')
+      assert.strictEqual(typeof blog.id, 'string')
+      assert.strictEqual(blog._id, undefined, 'blog should not have "_id"')
+    }
+})  
+
+test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: "Test Blog",
+      author: "Anna Sokolova",
+      url: "http://example.com/test",
+      likes: 9
+    }
+  
+    const blogsAtStart = await Blog.find({})
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await Blog.find({})
+    const titles = blogsAtEnd.map(b => b.title)
+  
+    assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+    assert.ok(titles.includes(newBlog.title))
+})
+
+test('blog without "likes" defaults to 0', async () => {
+    const newBlog = {
+      title: "No Likes Blog",
+      author: "Anna Sokolova",
+      url: "http://example.com/nolikes"
+      // no "likes" field
+    }
+  
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const savedBlog = response.body
+    assert.strictEqual(savedBlog.likes, 0)
+})
+
+test('blog without title is rejected with 400', async () => {
+    const newBlog = {
+      author: "Anna Sokolova",
+      url: "http://example.com/missingtitle",
+      likes: 1
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+  
+  test('blog without url is rejected with 400', async () => {
+    const newBlog = {
+      title: "Missing URL Blog",
+      author: "Anna Sokolova",
+      likes: 1
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+})  
+  
 test.after(async () => {
   await mongoose.connection.close()
 })
