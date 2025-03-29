bbe428d2787421b174c310dc97cdd586864e8a8e
Anna Sokolova <a.sokolova.dev@gmail.com>
2025-03-29 13:08:41 +0200

    refactor(part4): separate blog endpoints into controller

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,61 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-const Blog = require('./models/blog')
 require('dotenv').config()
 require('./mongo')
 
 const middleware = require('./utils/middleware')
 const app = express()
 
+const blogRouter = require('./controllers/blogs')
+
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.get('/api/blogs', async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs)
-})
+app.use('/api/blogs', blogRouter)
 
-app.post('/api/blogs', async (request, response, next) => {
-  try {
-    const blog = new Blog(request.body)
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
-  } catch (error) {
-    next(error)
-  }
-})
-
-app.delete('/api/blogs/:id', async (request, response, next) => {
-  try {
-    await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-  } catch (error) {
-    next(error)
-  }
-})
-
-app.put('/api/blogs/:id', async (request, response, next) => {
-  const { title, author, url, likes } = request.body
-
-  const updatedData = { title, author, url, likes }
-
-  try {
-    const updatedBlog = await Blog.findByIdAndUpdate(
-      request.params.id,
-      updatedData,
-      { new: true, runValidators: true, context: 'query' }
-    )
-
-    if (updatedBlog) {
-      response.json(updatedBlog)
-    } else {
-      response.status(404).end()
-    }
-  } catch (error) {
-    next(error)
-  }
-})
 
 app.use(middleware.unknownEndpoint) 
 app.use(middleware.errorHandler) 
---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const Blog = require('../models/blog')
+
+const blogRouter = express.Router()
+
+blogRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+blogRouter.post('/', async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body)
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (error) {
+    next(error)
+  }
+})
+
+blogRouter.put('/:id', async (request, response, next) => {
+  const { title, author, url, likes } = request.body
+  const updatedData = { title, author, url, likes }
+
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      request.params.id,
+      updatedData,
+      { new: true, runValidators: true, context: 'query' }
+    )
+
+    if (updatedBlog) {
+      response.json(updatedBlog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = blogRouter
