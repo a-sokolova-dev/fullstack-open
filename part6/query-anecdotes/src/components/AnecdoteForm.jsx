f6dbcfa6c2a3b3bd825d579cf0be95e57cad6a97
Anna Sokolova <a.sokolova.dev@gmail.com>
2025-04-12 14:51:09 +0300

    feat(part6): add exercise 6.24

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,4 +1,6 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useContext } from 'react';
+import NotificationContext from '../NotificationContext';
 
 const addAnecdote = async (newAnecdote) => {
   const response = await fetch('http://localhost:3001/anecdotes', {
@@ -18,6 +20,7 @@ const addAnecdote = async (newAnecdote) => {
 
 const AnecdoteForm = () => {
   const queryClient = useQueryClient();
+  const [notification, dispatch] = useContext(NotificationContext);
 
   const newAnecdoteMutation = useMutation({
     mutationFn: addAnecdote,
@@ -26,14 +29,27 @@ const AnecdoteForm = () => {
         ...oldData,
         newAnecdote,
       ]);
+      dispatch({ type: 'SET_NOTIFICATION', message: `Anecdote added: "${newAnecdote.content}"` });
+      setTimeout(() => {
+        dispatch({ type: 'CLEAR_NOTIFICATION' });
+      }, 5000);
     },
+    onError: (error) => {
+      dispatch({ type: 'SET_NOTIFICATION', message: `Error: ${error.message}` });
+      setTimeout(() => {
+        dispatch({ type: 'CLEAR_NOTIFICATION' });
+      }, 5000);
+    }
   });
 
   const onCreate = (event) => {
     event.preventDefault();
     const content = event.target.anecdote.value;
     if (content.length < 5) {
-      alert('Content must be at least 5 characters long');
+      dispatch({ type: 'SET_NOTIFICATION', message: 'too short anecdote, must have length 5 or more' });
+      setTimeout(() => {
+        dispatch({ type: 'CLEAR_NOTIFICATION' });
+      }, 5000);
       return;
     }
     event.target.anecdote.value = '';
